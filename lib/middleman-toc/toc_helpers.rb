28693ad51dea4c35f9f344b98db0dabdc6c129e0
Anton Fefilov <anton.fefilov@macrocoders.com>
2017-05-04 18:52:02 +0400

    combine chapters and guides modules

--- lib/middleman-toc/toc_helpers.rb
+++ lib/middleman-toc/toc_helpers.rb
@@ -1,11 +1,9 @@
 require "middleman-toc/toc_helpers/headers"
 require "middleman-toc/toc_helpers/titles"
-require "middleman-toc/toc_helpers/chapters"
-require "middleman-toc/toc_helpers/guides"
+require "middleman-toc/toc_helpers/sections"
 require "middleman-toc/toc_helpers/main"
 
 include TocHelpers::Headers
 include TocHelpers::Titles
-include TocHelpers::Chapters
-include TocHelpers::Guides
+include TocHelpers::Sections
 include TocHelpers::Main
--- lib/middleman-toc/toc_helpers/sections.rb
+++ lib/middleman-toc/toc_helpers/sections.rb
@@ -1,5 +1,35 @@
 module TocHelpers
-  module Chapters
+  module Sections
+    def previous_guide
+      return unless current_guide
+
+      current_guide_index = data.pages.find_index(current_guide)
+      return unless current_guide_index
+
+      previous_guide_index = current_guide_index - 1
+
+      if previous_guide_index >= 0
+        data.pages[previous_guide_index]
+      else
+        nil
+      end
+    end
+
+    def next_guide
+      return unless current_guide
+
+      current_guide_index = data.pages.find_index(current_guide)
+      return unless current_guide_index
+
+      next_guide_index = current_guide_index + 1
+
+      if current_guide_index < data.pages.length
+        data.pages[next_guide_index]
+      else
+        nil
+      end
+    end
+
     def chapter_links
       %Q{
       <footer>
@@ -82,6 +112,21 @@ module TocHelpers
     end
 
     private
+
+    def current_guide
+      return @current_guide if @current_guide
+
+      path = current_page.path.gsub('.html', '')
+      guide_path = path.split("/")[0]
+
+      @current_guide = data.pages.find do |guide|
+        guide.url == guide_path
+      end
+    end
+
+    def current_guide_index
+      data.pages.find_index(current_guide)
+    end
 
     def current_chapter
       return unless current_guide
